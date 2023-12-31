79cc685a394cd008cc84d4a670e8f7c8615d3380
venkata-suresh-bukka <155045637+venkata-suresh-bukka@users.noreply.github.com>
2023-12-31 16:09:25 +0530

    my portfolio

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,53 @@
+export const data = {
+    experience: [
+      {
+        title: "Associate Professional Software Engineer,",
+        subTitle: "Cloud Engineer & DevOps on GCP",
+        description:
+          "Engineered serverless CI/CD pipelines on Google Cloud Platform (GCP) by leveraging Cloud Run, Cloud Build, and Terraform, significantly enhancing the deployment efficiency for containerized applications. Successfully deployed applications on AppEngine through Cloud Build, demonstrating proficiency in streamlined and automated deployment processes. Possess hands-on experience in migrating on-premises VMs and databases to GCP VMs and GKE, showcasing expertise in seamless cloud migration.",
+      },
+      {
+        title: "Associate Professional Software Engineer,",
+        subTitle: "Cloud Developer & Automation GCP",
+        description:
+          "Orchestrated end-to-end deployment automation for Dockerized applications on a virtual machine using Terraform, Nginx, and Cloud Build, ensuring smooth deployment to both Cloud Run and GKE. Devised scalable Terraform templates for the deployment of multi-service architectures on Google Cloud Platform (GCP). Implemented Python scripts along with Cloud Functions for automating the scanning and identification of Personally Identifiable Information (PII) columns within a BigQuery dataset, efficiently pushing the results to the designated target dataset.",
+      },
+      {
+        title: "Analyst II Cloud Engineering at DXC Technology",
+        subTitle: "Cloud Automation Engineer on AWS",
+        description: [
+          "Automated the end-to-end deployment of a Django and React application as separate Docker containers in the AWS environment.",
+          "Incorporated Route 53 for domain management and Nginx configurations for web server functionality.",
+          "Implemented a robust CI/CD pipeline utilizing Jenkins Enterprise and native AWS tools through Terraform.",
+          "Ensured seamless communication between the backend and frontend containers for efficient API calls within the application.",
+          "Developed an automated email reporting system.",
+        ],
+      },
+      {
+        title: "Analyst II Cloud Engineering at DXC Technology",
+        subTitle: "Full Stack Developer",
+        description:
+          "Developed a health check dashboard using ReactJs and Django for monitoring services and servers in the Cloud environment. Containerized the application and deployed it on Cloud Run, ensuring scalability and accessibility which will save 95% of the manual efforts. Integrated Enterprise Azure AD+Okta+SSO to react application for handling authentication.",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary,",
+        subTitle: "Sri Mutyala Subbana Memorial Z P High School",
+        description:
+          "8.2 GPA(2015)",
+      },
+      {
+        title: "Diploma",
+        subTitle: "Aditya Polytechnic College,",
+        description:
+          "Computer Maintaince Engineerig -- 8.2 CGPA(2018)",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Aditya Engineering College",
+        description:
+          "Bachelor of technology in computer science -- 7.9 CGPA(2021)",
+      },
+    ],
+  };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,143 @@
+export const skillsData = [
+  {
+    label: "GCP Services",
+    data: [
+      {
+        skillName: "Compute Engine",
+        percentage: "90",
+      },
+      {
+        skillName: "VM & DB Migration Tools",
+        percentage: "70",
+      },
+      {
+        skillName: "GKE",
+        percentage: "50",
+      },
+      {
+        skillName: "Cloud Run",
+        percentage: "80",
+      },
+      {
+        skillName: "Cloud Build",
+        percentage: "80",
+      },
+      {
+        skillName: "Big Query",
+        percentage: "70",
+      },
+      {
+        skillName: "Cloud Function",
+        percentage: "80",
+      },
+      {
+        skillName: "Cloud Scheduler",
+        percentage: "80",
+      },
+    ],
+  },
+  {
+    label: "AWS Services",
+    data: [
+      {
+        skillName: "Ec2",
+        percentage: "70",
+      },
+      {
+        skillName: "Fargate",
+        percentage: "70",
+      },
+      {
+        skillName: "RDS",
+        percentage: "85",
+      },
+      {
+        skillName: "lambda",
+        percentage: "85",
+      },
+      {
+        skillName: "Step funcitons",
+        percentage: "85",
+      },
+      {
+        skillName: "Secret Manager",
+        percentage: "85",
+      },
+      {
+        skillName: "SNS",
+        percentage: "85",
+      },
+      {
+        skillName: "Route 53",
+        percentage: "65",
+      },
+    ],
+  },
+  {
+    label: "DevOps",
+    data: [
+      {
+        skillName: "Docker",
+        percentage: "70",
+      },
+      {
+        skillName: "Jenkins",
+        percentage: "70",
+      },
+      {
+        skillName: "GitHub",
+        percentage: "85",
+      },
+      {
+        skillName: "Terraform",
+        percentage: "85",
+      },
+      {
+        skillName: "HashiCorp Vault",
+        percentage: "50",
+      },
+      {
+        skillName: "Nginx",
+        percentage: "85",
+      },
+    ],
+  },
+    {
+      label: "Web Dev",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "50",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+        {
+          skillName: "Django",
+          percentage: "50",
+        },
+        {
+          skillName: "React",
+          percentage: "50",
+        },
+      ],
+    },
+    {
+      label: "Programming",
+      data: [
+        {
+          skillName: "PYTHON",
+          percentage: "65",
+        },
+        {
+          skillName: "SQL",
+          percentage: "55",
+        },
+      ],
+    },
+  ];
