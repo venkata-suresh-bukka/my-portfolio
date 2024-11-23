2e9a95dc51cae8cbe25dceea6b99b00e8803dadf
venkata-suresh-bukka <155045637+venkata-suresh-bukka@users.noreply.github.com>
2024-11-23 10:26:51 +0530

    deployment

--- deployment/Iac_terraform/modules/pubsub/main.tf
+++
@@ -1,6 +0,0 @@
-#Pubsub_notification..............................................................................
-resource "google_pubsub_topic" "topic" {
-  project = var.project_id
-  name    = var.pubsub_topic
-
-}
